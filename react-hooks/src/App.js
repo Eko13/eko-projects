6c8bc4ee1db4f24118ddf4b571d66f25b50d9920
«deadpoollkgz312» <deadpollkgz@gmail.com>
2020-07-29 17:06:50 +0600

    fix codes, fix react-app

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from "./todos/TodoList";
 
 function App() {
   return (
     <div className="App">
 
-
+      <TodoList/>
 
     </div>
   );
