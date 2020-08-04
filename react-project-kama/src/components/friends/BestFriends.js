4ee86bf9bf32d6e72d6fae354ab0c7c748639a2f
Eko13 <eko13.97@gmail.com>
2020-08-04 20:00:19 +0600

    fix some styles

--- react-project-kama/src/components/friends/BestFriends.js
+++ react-project-kama/src/components/friends/BestFriends.js
@@ -6,17 +6,18 @@ const BestFriends = props => {
 
   return (
     <div className="best-friends __wrap">
-        {state.bestFriends.rowBestFriends.map((bf, index) => (
-          <div className="friend-item __wrap"
-               key={`BEST_FRIEND_ITEM_${index}`}
-          >
-            <div className="friend-item __image">
-              <img src={bf.avatarImage} alt="Avatar"/>
-            </div>
-            <div className="friend-item __name">{bf.personName}</div>
+      {state.bestFriends.rowBestFriends.map((bf, index) => (
+        <div className="friend-item __wrap"
+             key={`BEST_FRIEND_ITEM_${index}`}
+        >
+          <div className="friend-item __image">
+            <img src={bf.avatarImage} alt="Avatar"/>
           </div>
-        ))
-        }
+          <div className="friend-item __name">
+            {bf.personName}
+          </div>
+        </div>
+      ))}
     </div>
   )
 };
