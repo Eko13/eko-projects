a876c784f61dcfb3b4d5629eaa1aa074fe7ffe9d
Eko13 <eko13.97@gmail.com>
2021-02-03 09:36:29 +0600

    Create commit

---
+++ JavaScript lessons/js-lessons-learn-js/js/lesson1/js1.js
@@ -0,0 +1,20 @@
+// let number = 5;
+//
+// function factorial(n) {
+//   let i = 0;
+//   let j = n;
+//   let sum;
+//
+//   for (i; i <= n; i++) {
+//     sum = n * (n - i);
+//   }
+//   console.log("факториал равен =>", sum);
+// }
+//
+// factorial(number);
+
+
+// n! = n * (n - 1) * (n - 2) * ...*1
+
+
+
